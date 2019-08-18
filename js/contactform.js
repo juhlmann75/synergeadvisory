9f2c19462cc6b6b5f51c44d3635122fa3f350082
Jonathan <juhlmann75@gmail.com>
2019-08-17 21:14:41 -0500

    test form

--- js/contactform.js
+++ js/contactform.js
@@ -99,6 +99,7 @@ jQuery(document).ready(function($) {
       url: action,
       data: str,
       success: function(msg) {
+		  console.log(msg);
         if (msg == 'OK') {
           $("#sendmessage").addClass("show");
           $("#errormessage").removeClass("show");
