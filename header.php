9e6d84659b3b668f84d77d13d03d228b6cc10f4d
Jonathan <juhlmann75@gmail.com>
2019-09-29 21:11:30 -0500

    service menu

--- header.php
+++ header.php
@@ -1,24 +1,56 @@
-<!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Synerge Advisory</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <?php require 'googleanalytics.php'; ?>
+  <title>Synergōs Advice</title>
+  <meta charset="utf-8" />
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- css -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,300,700,800" rel="stylesheet" media="screen">
+
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <link href="css/style.css" rel="stylesheet" media="screen">
+  <link href="css/default.css" rel="stylesheet" media="screen">
+  
+  <style>
+	.navlink a:hover {
+		background-color: transparent !important;
+		color:#3a3a3a;
+	}
+	.navlink {
+		text-transform: uppercase;
+		font-weight:bold;
+		font-size:14px !important;
+	}
+  </style>
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="staticNav" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">Toggle nav</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="index.html">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact</a>
-            </li>
-          </ul>
-        </div>
+
+        <!-- Logo text or image -->
+        <a class="navbar-brand" href="index.php"><img src="images/synergoslogo.PNG" style="width:260px;margin-top:-20px;"/></a>
+
       </div>
-    </nav>
+      <div class="navigation collapse navbar-collapse navbar-ex1-collapse staticLinks">
+        <ul class="nav navbar-nav" style="margin: 7.5px -15px !important;">
+          <li class="navlink"><a href="/index.php">Home</a></li>
+          <li class="navlink"><a href="/index.php#about">About</a></li>
+          <li class="navlink"><a href="/index.php#services">Service</a></li>
+          <li class="navlink"><a href="/index.php#contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
