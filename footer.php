ff37e9d1c98308345ca0c1955e48b53f9372731f
Jonathan Uhlmann <juhlma3@uic.edu>
2020-08-29 22:45:36 -0500

    changed footer text and added image

--- footer.php
+++ footer.php
@@ -4,10 +4,24 @@
       <div class="row">
         <div class="col-md-12" style="font-size:8pt;text-align:left;">
           <p>
-           Synergos Advisory LLC doing business as Synergos Advisory is a registered investment adviser in the state of Washington. 
-		   The adviser may not transact business in states where it is not appropriately registered or exempt from registration. I
-		   ndividualized responses to persons that involve either the effecting of transactions in securities or the rendering of 
-		   personalized investment advice for compensation will not be made without registration or exemption.</p>
+           Synergos Advisory LLC (“Synergos”) doing business as Synergos Advisory is a registered investment advisor offering advisory services in the State of Washington and in other jurisdictions where exempted. Registration does not imply a certain level of skill or training.
+
+The information on this site is not intended as tax, accounting or legal advice, as an offer or solicitation of an offer to buy or sell, or as an endorsement of any company, security, fund, or other securities or non-securities offering. This information should not be relied upon as the sole factor in an investment making decision.
+
+Past performance is no indication of future results. Investment in securities involves significant risk and has the potential for partial or complete loss of funds invested. It should not be assumed that any recommendations made will be profitable or equal any performance noted on this site.
+
+The information on this site is provided “AS IS” and without warranties of any kind either express or implied. To the fullest extent permissible pursuant to applicable laws, Synergos Advisory LLC disclaims all warranties, express or implied, including, but not limited to, implied warranties of merchantability, non-infringement, and suitability for a particular purpose.
+
+Synergos does not warrant that the information on this site will be free from error. Your use of the information is at your sole risk. Under no circumstances shall Synergos be liable for any direct, indirect, special or consequential damages that result from the use of, or the inability to use, the information provided on this site, even if Synergos or a Synergos authorized representative has been advised of the possibility of such damages. Information contained on this site should not be considered a solicitation to buy, an offer to sell, or a recommendation of any security in any jurisdiction where such offer, solicitation, or recommendation would be unlawful or unauthorized.
+
+The information being provided is strictly as a courtesy/convenience. When you link to any of the web sites provided here, you are leaving this website. We make no representation as to the completeness or accuracy of information provided at these websites.
+
+Synergos Advisory LLC is not liable for any direct or indirect technical or system issues or any consequences arising out of your access to or your use of third-party technology, web sites, information and programs made available through this website.
+
+When you access one of these web sites, you are leaving our web site and assume total responsibility and risk for use of the web sites you are visiting.
+
+Synergos Advisory LLC does not make any representations or warranties as to the accuracy, timeliness, suitability, completeness, or relevance of any information prepared by any unaffiliated third party, whether linked to Synergos Advisory LLC’s web site or incorporated herein, and takes no responsibility thereof.
+			</p>
             <ul>
                 <li><a href="files/Privacy_Policy.pdf" target="_blank">Privacy Policy & Disclaimer</a></li>
                 <li><a href="files/ADV_Part_2A_2B.pdf" target="_blank">Form ADV Part 2A</a></li>
